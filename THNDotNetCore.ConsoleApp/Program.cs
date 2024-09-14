a3eb62fdb5ff42ba3a933b318591da0964af17bc
Tin Htet Naing <tinhtet1011@gmail.com>
2024-09-14 22:23:56 +0630

    feat: ado.net read

--- THNDotNetCore.ConsoleApp/Program.cs
+++ THNDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+//Console.ReadKey();
+
+//max con => 100
+//not close => connection time our err
+
+string connectionString = "Data Source=DESKTOP-EPUBLLL; Initial Catalog=MyDB; User ID = sa; Password= sa@123;";
+SqlConnection connection = new SqlConnection(connectionString);
+Console.WriteLine("Connection Opening");
+connection.Open();
+Console.WriteLine("Connection Opened");
+
+string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[TBL_BLOG] where DeleteFlag = 0";
+
+SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
+
+SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    Console.WriteLine(reader["BlogId"]);
+    Console.WriteLine(reader["BlogTitle"]);
+    Console.WriteLine(reader["BlogAuthor"]);
+    Console.WriteLine(reader["BlogContent"]);
+}
+
+Console.WriteLine("Connection Closing");
+connection.Close();
+Console.WriteLine("Connection Closed");
+
+//foreach ( DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine(dr["BlogId"]);
+//    Console.WriteLine(dr["BlogTitle"]);
+//    Console.WriteLine(dr["BlogAuthor"]);
+//    Console.WriteLine(dr["BlogContent"]);
+//}
+
 Console.ReadKey();
