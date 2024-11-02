03f12b94ad10f24f47a02bec5ca9bacbd3ed04e9
Tin Htet Naing <tinhtetnaing.dev@gmail.com>
2024-11-02 14:16:18 +0630

    AdoDotNetApi

--- THNDotNetCore.RestApi/Controllers/BlogsAdoDotNetController.cs
+++ THNDotNetCore.RestApi/Controllers/BlogsAdoDotNetController.cs
@@ -46,19 +46,72 @@ namespace THNDotNetCore.RestApi.Controllers
         [HttpGet("{id}")]
         public IActionResult GetBlog(int id)
         {
-            
+            BlogViewModel item = new BlogViewModel();
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            string query = @"SELECT [BlogId]
+              ,[BlogTitle]
+              ,[BlogAuthor]
+              ,[BlogContent]
+              ,[DeleteFlag]
+          FROM [dbo].[TBL_BLOG] where DeleteFlag = 0 And BlogId = @BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                item = new BlogViewModel
+                {
+                    Id = Convert.ToInt32(reader["BlogId"]),
+                    Title = Convert.ToString(reader["BlogTitle"]),
+                    Author = Convert.ToString(reader["BlogAuthor"]),
+                    Content = Convert.ToString(reader["BlogContent"]),
+                    DeleteFlag = Convert.ToBoolean(reader["DeleteFlag"])
+                };
+            }
+            return Ok(item);
         }
 
         [HttpPost]
         public IActionResult CreateBlog(TblBlog blog)
         {
-           
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            string query = @"INSERT INTO [dbo].[TBL_BLOG]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+		   ,@BlogAuthor
+           ,@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", blog.BlogTitle);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blog.BlogAuthor);
+            cmd.Parameters.AddWithValue("@BlogContent", blog.BlogContent);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            return Ok(result > 0 ? "Create Successful" : "Create Fail");
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBlog(int id, TblBlog blog)
         {
-            
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            string query = @"UPDATE [dbo].[TBL_BLOG]
+           SET [BlogTitle] = @BlogTitle
+              ,[BlogAuthor] = @BlogAuthor
+              ,[BlogContent] = @BlogContent
+         WHERE BlogId = @BlogId;";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", blog.BlogId);
+            cmd.Parameters.AddWithValue("@BlogTitle", blog.BlogTitle);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blog.BlogAuthor);
+            cmd.Parameters.AddWithValue("@BlogContent", blog.BlogContent);
+            var result = cmd.ExecuteNonQuery();
+            connection.Close();
+            return Ok(result > 0 ? "Update Successful" : "Update Fail");
         }
 
         [HttpPatch]
@@ -67,15 +120,15 @@ namespace THNDotNetCore.RestApi.Controllers
             string condition = "";
             if (string.IsNullOrEmpty(blog.Title))
             {
-                condition += " [BlogTitle] = @BlogTitle ";
+                condition += " [BlogTitle] = @BlogTitle, ";
             }
             if (string.IsNullOrEmpty(blog.Author))
             {
-                condition += " [BlogAuthor] = @BlogAuthor ";
+                condition += " [BlogAuthor] = @BlogAuthor, ";
             }
-            if (string.IsNullOrEmpty(blog.Title))
+            if (string.IsNullOrEmpty(blog.Content))
             {
-                condition += " [BlogContent] = @BlogContent ";
+                condition += " [BlogContent] = @BlogContent, ";
             }
 
             if(condition.Length == 0)
