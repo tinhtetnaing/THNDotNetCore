9aad50a72edac31480a0beeefa4ee4b199168093
Tin Htet Naing <tinhtetnaing.dev@gmail.com>
2024-11-25 12:33:09 +0630

    feat: add HttpClient ConsoleApp

---
+++ THNDotNetCore.HttpClientConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+if (response.IsSuccessStatusCode)
+{
+    string jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(jsonStr);
+}
