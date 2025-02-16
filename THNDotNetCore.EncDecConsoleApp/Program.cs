42a546e55421b1c382ae30f19ac06382eab60f0c
Tin Htet Naing <tinhtetnaing.dev@gmail.com>
2025-02-16 23:55:10 +0630

    feat: add EncDec console app

---
+++ THNDotNetCore.EncDecConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Effortless.Net.Encryption;
+
+byte[] key = Bytes.GenerateKey();
+byte[] iv = Bytes.GenerateIV();
+string encrypted = Strings.Encrypt("Secret", key, iv);
+string decrypted = Strings.Decrypt(encrypted, key, iv);
+
+Console.WriteLine("Key: "+key.ToString());
+Console.WriteLine("iv: " + iv.ToString());
+Console.WriteLine(encrypted);
+Console.WriteLine(decrypted);
